1492144a757d586a243a54400c3bf0eff6ab4c6a
unknown <sandeep.misra96@gmail.com>
2020-12-02 10:56:25 +0530

    Updates

---
+++ precache-manifest.01e5076bf7b144215ffaa4fd77a8aa8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f815d412e4030eba0d0abf5fef4511a",
+    "url": "/grocery/index.html"
+  },
+  {
+    "revision": "0c398a3de9c28cc17eff",
+    "url": "/grocery/static/css/main.21454903.chunk.css"
+  },
+  {
+    "revision": "87278c57efefa72ab331",
+    "url": "/grocery/static/js/2.4d995c5c.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/grocery/static/js/2.4d995c5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c398a3de9c28cc17eff",
+    "url": "/grocery/static/js/main.472c2b3c.chunk.js"
+  },
+  {
+    "revision": "878ace7b3997042414af",
+    "url": "/grocery/static/js/runtime-main.9cdd7133.js"
+  },
+  {
+    "revision": "b2c7a617e5466aefb9b419b5718890bc",
+    "url": "/grocery/static/media/bg.b2c7a617.png"
+  }
+]);
